3c2de22b643a8f0abf60f39ce24620d3d8db824e
Nathnael Dereje <nathandere1357@gmail.com>
2023-05-04 15:46:04 +0300

    separated the signup_field_form in a file

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -5,18 +5,4 @@ void main() {
   runApp(const SignUpPage());
 }
 
-class Test extends StatelessWidget {
-  const Test({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("This is a test"),
-        ),
-        body: const Text("This is a test"),
-      ),
-    );
-  }
-}
--- frontend/lib/signup.dart
+++ frontend/lib/signup.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/header_banner.dart';
+import 'package:frontend/signup_field_form.dart';
 
 class SignUpPage extends StatelessWidget {
   const SignUpPage({Key? key}) : super(key: key);
@@ -7,48 +8,17 @@ class SignUpPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SizedBox(
-        child: Column(
-          children: const <Widget>[
-            HeaderBanner(),
-            FieldForm(),
-            // Below is the signup form.
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class FieldForm extends StatelessWidget {
-  const FieldForm({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      child: Form(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const TextField(
-              decoration: InputDecoration(
-                hintText: 'Enter your email',
-              ),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: SizedBox(
+            child: Column(
+              children: const <Widget>[
+                HeaderBanner(),
+                SignUpFieldForm(),
+                // Below is the signup form.
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  // Validate will return true if the form is valid, or false if
-                  // the form is invalid.
-                },
-                child: const Text('Create Account'),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
