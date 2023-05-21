f0966c5932bbbfc26077c44b2116d0243e850839
Nebiyou Daniel <nebiyoudanielnd@gmail.com>
2023-05-20 17:33:59 -0700

    I have made 4 new files: two custom widgets known as line_chart and date_picker, and two screen pages traineeProgressPage and workoutPlanCreationPage. ALso modified he pubspec.yaml file to install the flutter charts dependency for the line charts. Further work required for line Chart widget and I am yet to do the trainee Progress Page.

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:frontend/screen/common/about.dart';
 import 'package:frontend/screen/adminSignupAndLogin.dart';
 import 'package:frontend/screen/common/signup.dart';
+import 'package:frontend/screen/traineeProgressPage.dart';
+import 'package:frontend/screen/workoutPlanCreationPage.dart';
 
 void main() {
+  // runApp(const WorkoutPlanCreationPage());
+  // runApp(const AboutPage());
   runApp(const SignUpPage());
+  // runApp(const AdminSignupAndLogin());
+  // runApp(const TraineeProgressPage());
+
 }
 
